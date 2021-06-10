00dc6de7fa83a43c80e0acbd679f91dcd2205494
Jonas <geigerjonas3@gmail.com>
2021-06-10 22:41:48 +0200

    Pr erstellt

---
+++ Bobii/CommandHandler.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord.Commands;
+using Discord.WebSocket;
+using Newtonsoft.Json.Linq;
+using Discord;
+using System.Linq;
+using Newtonsoft.Json;
+using Bobii.src.Functions;
+
+namespace Bobii
+{
+    public class CommandHandlingService
+    {
+        private readonly CommandService _commands;
+        private readonly DiscordSocketClient _client;
+        private readonly IServiceProvider _services;
+
+        public CommandHandlingService(IServiceProvider services)
+        {
+
+            _commands = services.GetRequiredService<CommandService>();
+            _client = services.GetRequiredService<DiscordSocketClient>();
+            _services = services;
+
+            // Event handlers
+            _client.Ready += ClientReadyAsync;
+            _client.MessageReceived += HandleCommandAsync;
+            _client.JoinedGuild += SendJoinMessageAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage rawMessage)
+        {
+            if (rawMessage.Author.IsBot || !(rawMessage is SocketUserMessage message) || message.Channel is IDMChannel)
+                return;
+
+            var context = new SocketCommandContext(_client, message);
+
+            int argPos = 0;
+
+            JObject config = Functions.GetConfiguration();
+            string[] prefixes = JsonConvert.DeserializeObject<string[]>(config["prefixes"].ToString());
+
+            // Check if message has any of the prefixes or mentiones the bot.
+            if (prefixes.Any(x => message.HasStringPrefix(x, ref argPos)) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                // Execute the command.
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess && result.Error.HasValue)
+                    await context.Channel.SendMessageAsync($":x: {result.ErrorReason}");
+            }
+        }
+
+        private async Task SendJoinMessageAsync(SocketGuild guild)
+        {
+            JObject config = Functions.GetConfiguration();
+            string joinMessage = config["join_message"]?.Value<string>();
+
+            if (string.IsNullOrEmpty(joinMessage))
+                return;
+
+            // Send the join message in the first channel where the bot can send messsages.
+            foreach (var channel in guild.TextChannels.OrderBy(x => x.Position))
+            {
+                var botPerms = channel.GetPermissionOverwrite(_client.CurrentUser).GetValueOrDefault();
+
+                if (botPerms.SendMessages == PermValue.Deny)
+                    continue;
+
+                try
+                {
+                    await channel.SendMessageAsync(joinMessage);
+                    return;
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+        }
+
+        private async Task ClientReadyAsync()
+            => await Functions.SetTheBotStatus(_client);
+
+        public async Task InitializeAsync()
+            => await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+    }
+}
---
+++ Bobii/src/Functions/Functions.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bobii.src.Functions
+{
+    public static class Functions
+    {
+        public static async Task SetTheBotStatus(DiscordSocketClient client)
+        {
+            await client.SetActivityAsync(new Game("BobSty", ActivityType.Listening));
+            await client.SetStatusAsync(UserStatus.Online);
+            Console.WriteLine($"{DateTime.Now.TimeOfDay:hh\\:mm\\:ss} Status wurde erfolgreich gesetzt.");
+        }
+
+        public static JObject GetConfiguration()
+        {
+            using StreamReader configJson = new StreamReader(Directory.GetCurrentDirectory() + @"/Config.json");
+            return (JObject)JsonConvert.DeserializeObject(configJson.ReadToEnd());
+        }
+    }
+}
