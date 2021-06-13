41b4dc12ea1cf02097326bf13467046e5ed4b4ce
Jonas <geigerjonas3@gmail.com>
2021-06-13 17:08:29 +0200

    Pr erstellt

---
+++ Bobii/src/Commands/Commands.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bobii.src.Commands
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("help")]
+        [Summary("Summary of all my commands")]
+        public async Task Help()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("**Icon**");
+            sb.Append("Shows your Avatar");
+            EmbedBuilder embed = new EmbedBuilder()
+                .WithColor(0, 225, 225)
+                 .WithDescription(
+                     sb.ToString());
+            await ReplyAsync(embed: embed.Build());
+            Console.WriteLine($"{DateTime.Now.TimeOfDay:hh\\:mm\\:ss} Commands    'help was used by {Context.User}");
+        }
+    }
+}
