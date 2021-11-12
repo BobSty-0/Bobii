59559aa295f3f1012e018bfe555a40985b28002d
BobSty <jgeiger@novasoftware.de>
2021-11-12 18:10:09 +0100

    created everything for entity framework

---
+++ Bobii/src/EntityFramework/Entities/filterlinkoptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bobii.src.EntityFramework.Entities
+{
+    class filterlinkoptions
+    {
+        [Key]
+        public int id { get; set; }
+        [MaxLength(50)]
+        public string bezeichnung { get; set; }
+        [MaxLength(100)]
+        public string linkbody { get; set; }
+    }
+}
---
+++ Bobii/src/EntityFramework/Entities/filterwords.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bobii.src.EntityFramework.Entities
+{
+    class filterwords
+    {
+        [Key]
+        public int id { get; set; }
+        [MaxLength(18)]
+        public ulong guildid { get; set; }
+        [MaxLength(20)]
+        public string filterword { get; set; }
+        [MaxLength(20)]
+        public string replaceword { get; set; }
+    }
+}
