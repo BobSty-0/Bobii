6a15b9ddb66e1bddd3af8d3c1ee5dc332383c04b
BobSty <jgeiger@novasoftware.de>
2022-09-17 11:45:13 +0200

    Fixed the order of the commands

--- Bobii/src/Handler/EventHandler.cs
+++ Bobii/src/Handler/EventHandler.cs
@@ -276,7 +276,7 @@ namespace Bobii.src.Handler
                 else
                 {
                     //await _interactionService.RegisterCommandsGloballyAsync(true);
-                    //await _interactionService.AddModulesGloballyAsync(false, _interactionService.GetModuleInfo<CreateTempChannelSlashCommands>());
+                    await _interactionService.AddModulesGloballyAsync(false, _interactionService.GetModuleInfo<CreateTempChannelSlashCommands>());
                     await _interactionService.AddModulesGloballyAsync(false, _interactionService.GetModuleInfo<TempChannelSlashCommands>());
                     //await _interactionService.AddModulesGloballyAsync(false, _interactionService.GetModuleInfo<HelpShlashCommands>());
                     //await _interactionService.AddModulesGloballyAsync(false, _interactionService.GetModuleInfo<TextUtilitySlashCommands>());
