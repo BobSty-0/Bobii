dffe71b11bedeeceef1c655d0198a72c500fafea
bobsty <78264479+BobSty-0@users.noreply.github.com>
2026-03-05 19:15:32 +0100

    Lng Migration hinzugefügt

---
+++ bobii_rework/Migrations/BobiiLng/20260305181513_Init.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace bobii_rework.Migrations.BobiiLng
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Captions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    msgid = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    en = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    de = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ru = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Captions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Commands",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    command = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    en = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    de = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ru = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commands", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contents",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    msgid = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    en = table.Column<string>(type: "character varying(400)", maxLength: 400, nullable: false),
+                    de = table.Column<string>(type: "character varying(400)", maxLength: 400, nullable: false),
+                    ru = table.Column<string>(type: "character varying(600)", maxLength: 600, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contents", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    guildid = table.Column<decimal>(type: "numeric(20,0)", maxLength: 18, nullable: false),
+                    langugeshort = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Captions");
+
+            migrationBuilder.DropTable(
+                name: "Commands");
+
+            migrationBuilder.DropTable(
+                name: "Contents");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+        }
+    }
+}
